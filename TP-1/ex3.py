ed762134b72db2f5a5663d7d0ed75825ca961346
AbdoAMD <abdelghaniamejoud@gmail.com>
2024-12-10 18:16:51 +0100

    TP-1 done...

---
+++ TP-1/ex3.py
@@ -0,0 +1,17 @@
+def intersection(ensemble1, ensemble2):
+    
+    return intersection (ensemble1 , ensemble2) 
+
+
+ensemble1 = {1, 2, 3, 4}
+ensemble2 = {3, 4, 5, 6}
+resultat = ensemble1.intersection(ensemble2) 
+print("L'intersection des deux liste est : \n", resultat)
+
+
+
+
+
+
+
+
